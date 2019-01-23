7b59bff8fde31fad84ee80eee3e33a65e719e56b
Forgottenrealms <1456671936@qq.com>
2019-01-23 19:38:00 +0800

    feat: 新增商品详情上周销量数据图

--- src/pages/Product/Details/ProductDetails.js
+++ src/pages/Product/Details/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import {
   Card,
   Button,
@@ -6,21 +6,22 @@ import {
   Row,
   Col
 } from 'antd'
-import { getProductDetails } from '@/requests'
+import { getProductDetails, getProductWeekSales } from '@/requests'
+
+import moment from 'moment'
+
+const echarts = require('echarts')
+
 
 export default class ProductDetails extends Component {
   constructor() {
     super()
+    this.salesEcharts = createRef()
     this.state = {
       productDetails: {}
     }
   }
-  componentDidMount() {
-    // 获取要查询的商品id
-    const _id = this.props.match.params.id
-    console.log(_id)
-    this.fetchProductDetails(_id)
-  }
+  // 获取查看的商品详情
   fetchProductDetails = (id) => {
     getProductDetails(id)
       .then(res => {
@@ -35,98 +36,116 @@ export default class ProductDetails extends Component {
         }
       })
   }
+  // 初始化销售走势图echarts实例
+  initSalesEcharts = () => {
+    // 基于准备好的dom，初始化echarts实例
+    const salesChart = echarts.init(this.salesEcharts.current);
+    getProductWeekSales()
+      .then(res => {
+        if (res.data.code === "200") {
+          // console.log(res.data.data)
+          // 绘制图表
+          salesChart.setOption({
+            title: {
+                text: res.data.title
+            },
+            tooltip: {},
+            xAxis: {
+                data: res.data.data.map(item => item.week)
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: res.data.data.map(item => item.sales)
+            }]
+          });
+        }
+      })
+  }
+  componentDidMount() {
+    // 获取要查询的商品id
+    const _id = this.props.match.params.id
+    // console.log(_id)
+    this.fetchProductDetails(_id)
+    this.initSalesEcharts()
+  }
   render() {
-    console.log(this.state.productDetails)
+    // console.log(this.state.productDetails)
     return (
       <Card
-          title="商品详情 >"
-          extra={
-              <div>
-                  <Button type="primary">
-                      <Icon type="edit" />编辑
-                  </Button>
-              </div>
-          }
+        style={{backgroundColor: "#f0f2f5"}}
+        title={"商品详情 > " + this.state.productDetails.type}
+        headStyle={{
+          fontSize: "24px"
+        }}
+        extra={
+            <div>
+                <Button type="primary">
+                    <Icon type="edit" />编辑
+                </Button>
+            </div>
+        }
       > 
         <Row
-          type="flex" 
-          justify="space-between"
+          gutter={16}
         >
-          <Col 
-            span={7}
-            style={{
-              padding: '20px',
-              backgroundColor: '#dedede'
-            }}
+          <Col
+            span={6}
           >
-            <Card title="型号" bordered={false} style={{ width: 300 }}>
-              <p>{this.state.productDetails.type}</p>
+            <Card title="型号" bordered={false} style={{ width: 240 }}>
+              <h2>{this.state.productDetails.type}</h2>
             </Card>
           </Col>
-          <Col 
-            span={7}
-            style={{
-              padding: '20px',
-              backgroundColor: '#dedede'
-            }}
+          <Col
+            span={6}
           >
-            <Card title="售价" bordered={false} style={{ width: 300 }}>
-              <p>{this.state.productDetails.price}</p>
+            <Card title="售价" bordered={false} style={{ width: 240 }}>
+              <h2>{this.state.productDetails.price}</h2>
             </Card>
           </Col>
-          <Col 
-            span={7}
-            style={{
-              padding: '20px',
-              backgroundColor: '#dedede'
-            }}
+          <Col
+            span={6}
           >
-            <Card title="销量" bordered={false} style={{ width: 300 }}>
-              <p>{this.state.productDetails.sales}</p>
+            <Card title="销量" bordered={false} style={{ width: 240 }}>
+              <h2>{this.state.productDetails.sales}</h2>
+            </Card>
+          </Col>
+          <Col
+            span={6}
+          >
+            <Card title="简介" bordered={false} style={{ width: 240 }}>
+              <h2>{this.state.productDetails.desc}</h2>
             </Card>
           </Col>
         </Row>
         <Row
-          type="flex" 
-          justify="space-between"
-          style={{
-            marginTop: '50px'
-          }}
+          gutter={16}
+          style={{marginBottom: "24px"}}
         >
-          <Col 
-            span={7}
-            style={{
-              padding: '20px',
-              backgroundColor: '#dedede'
-            }}
+          <Col
+            span={6}
           >
-            <Card title="简介" bordered={false} style={{ width: 300 }}>
-              <p>{this.state.productDetails.desc}</p>
+            <Card title="库存" bordered={false} style={{ width: 240 }}>
+              <h2>{this.state.productDetails.amount}</h2>
             </Card>
           </Col>
-          <Col 
-            span={7}
-            style={{
-              padding: '20px',
-              backgroundColor: '#dedede'
-            }}
+          <Col
+            span={6}
           >
-            <Card title="库存" bordered={false} style={{ width: 300 }}>
-              <p>{this.state.productDetails.amount}</p>
-            </Card>
-          </Col>
-          <Col 
-            span={7}
-            style={{
-              padding: '20px',
-              backgroundColor: '#dedede'
-            }}
-          >
-            <Card title="上次更新" bordered={false} style={{ width: 300 }}>
-              <p>{this.state.productDetails.updateTime}</p>
+            <Card title="上次更新" bordered={false} style={{ width: 240 }}>
+              <h2>{moment(Number.parseInt(this.state.productDetails.updateTime, 10)).format("HH:mm:ss DD/MM/YY")}</h2>
             </Card>
           </Col>
         </Row>
+        <Card
+          title="销量"
+          headStyle={{
+            fontSize: "24px"
+          }}
+        > 
+          <div style={{height: "400px",width: "400px"}} ref={this.salesEcharts}></div>
+        </Card>
       </Card>
     )
   }
--- src/requests/index.js
+++ src/requests/index.js
@@ -11,12 +11,15 @@ const ajax2 = axios.create({
     // baseURL: isDEV ? "http://rap2api.taobao.org/app/mock/124859" : ""
     baseURL: isDEV ? "http://192.168.1.103:5000" : ""    // 本地mock数据接口
 })
-
+// 商品相关
 export const getProductTables = () => {
     return ajax.post("/api/product/tables")
 }
 export const getProductDetails = (id) => {
     return ajax.post(`/api/product/details/${id}`)
+}
+export const getProductWeekSales = (id) => {
+    return ajax.post(`/api/product/weeksales/${id}`)
 }
 // 获取用户数据
 export const getUsers = () => {
